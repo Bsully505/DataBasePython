65a14d2ad27345ca177eec15f05a68c503e111d2
Bryan Sullivan <Bsully505@yahoo.com>
2020-10-29 15:00:27 -0400

    testing if works as of right now

--- DataBase/example (1).py
+++ DataBase/example (1).py
@@ -1,11 +1,8 @@
 import pymysql as ps
 
+#AUTHORS BRYAN SULLIVAN and HENOK KELSELA
 
 
-# name db = yeast-transcriptomes
-# user_name = admin
-# password: qJ6D2qwzjhbdwic
-# make the connection to the db
 def make_connection():
     return ps.connect(host='bryandbruby.cucbdvc0bgxn.us-east-1.rds.amazonaws.com', user='admin',
                       passwd='admin123',
@@ -41,12 +38,12 @@ def setup_dp(cur):
         FOREIGN key (team) references Team(team) );''')
     cur.execute(
         '''CREATE TABLE Deliveries  (
-        Match_ID          int NOT NULL PRIMARY KEY,
-        inning            int          PRIMARY KEY,
-        Batting_Team      VARCHAR(50) Primary key,
-        Bowling_Team      VARCHAR(50) Primary key,
-        Over              int Primary key,
-        Ball              int Primary key,
+        Match_ID          Int NOT NULL PRIMARY KEY,
+        inning            Int          PRIMARY KEY,
+        Batting_Team      VARCHAR(50) PRMARY KEY,
+        Bowling_Team      VARCHAR(50) PRIMARY KEY,
+        Over              Int PRIMARY KEY,
+        Ball              Int PRIMARY KEY,
         Batsman           Varchar(50),
         Non_Striker       Varchar(50),
         Bowler            Varchar(50)
@@ -56,7 +53,7 @@ def setup_dp(cur):
         Match_ID INT NOT NULL PRIMARY KEY ,
         Season                  Varchar(50),
         City                    Varchar(50),
-        Date                    Date,
+        Date                    DATE,
         Team1                   Varchar(50),
         Team2                   Varchar(50),
         TossWnner               Varchar(50),
@@ -66,7 +63,7 @@ def setup_dp(cur):
     # Create Join Tables
     cur.execute(
         '''CREATE TABLE Team_Stats (
-        TeamS           Varchar(50) Not NULL Primary Key,
+        TeamS           Varchar(50) NOT NULL PRMARY KEY,
         HomeWins        Int,
         AwayWins        Int ,
         HomeMatches     Int,
@@ -74,8 +71,8 @@ def setup_dp(cur):
         );''')
     cur.execute(
         '''CREATE TABLE Team (
-        Team Varchar(20) not null Primary Key,
-        teamS varchar(50)
+        Team       Varchar(20) NOT NULL PRIMARY KEY,
+        teamS      Varchar(50)
         FOREIGN KEY (teamS) references Team_Stats(TeamS)
         );''')
 #we still need to make tables for player-Deliveries, Team-Deliveries, Deliveries-matches,Team-Matches
@@ -86,49 +83,62 @@ def insert_data(cur):
         next(r1)
         for line in r1:
             line = line.split(',')
-            ID_num = line.__getitem__(0)
-            sample_ID = line.__getitem__(1)
-            primary = line.__getitem__(2)
-            secondary = line.__getitem__(3)
-            additional_info = line.__getitem__(4)
-            # print(ID_num,sample_ID,primary,secondary,additional_info)
+                PlayerName = str(line.__getitem__(0))
+                DOB = line.__getitem__(1)
+                Batting_Hand = line.__getitem__(2)
+                Bowling_Skill = line.__getitem__(3)
+                Country = line.__getitem__(4)
+
             cur.execute(
-                'INSERT IGNORE INTO Player(,PrimaryComponent,SecondaryComponent,Additional_Information) VALUES (%s,%s,%s,%s)',
-                (sample_ID, primary, secondary, additional_info))
+                'INSERT IGNORE INTO Players VALUES (%s,%s,%s,%s,%s)',
+                (PlayerName, DOB, Batting_Hand, Bowling_Skill, country))
+            # print(ID_num,sample_ID,primary,secondary,additional_info)
     # insertions for Yeast-gene and Localization table and Yeast-gene&localization join table
     with open("data/deliveries.csv", 'r') as r1:
         # skips first line the headers
         next(r1)
         for line in r1:
             line = line.split(',')
-            ID_num = int(line.__getitem__(0)) + 1
-            ID_num = str(ID_num)
-            gene = line.__getitem__(1)
-            valid = line.__getitem__(2)
-            bp = line.__getitem__(3)
-            cc = line.__getitem__(4)
-            mf = line.__getitem__(5)
+            Match_ID = int(line.__getitem__(0))
+            Match_ID = str(ID_num)
+            inning = int(line.__getitem__(1))
+            inning = str(ID_num)
+            Batting_team = line.__getitem__(2)
+            Bowling_Team = line.__getitem__(3)
+            over = int(line.__getitem__(4))
+            over = str(over)
+            Ball = int(line.__getitem__(5))
+            Ball = str(Ball)
+            batsman = line.__getitem__(6)
+            non_striker = line.__getitem__(7)
+            bowler = line.__getitem__(8)
+
             cur.execute(
-                'INSERT IGNORE INTO Yeast_Gene(Gene_ID,Validation, Biological_Process,Cellular_Component,Molecular_Function) VALUES (%s,%s,%s,%s,%s)',
-                (gene, valid, bp, cc, mf))
-            cur.execute(
-                'INSERT IGNORE INTO Localization(Localization_ID,Biological_Process_Loc,Cellular_Component_Loc,Molecular_Function) VALUES (%s,%s,%s,%s)',
-                (ID_num, bp, cc, mf))
-            cur.execute('INSERT IGNORE INTO YeastGene_Localization(Gene_ID,Localization_ID) VALUES (%s,%s)', (gene, ID_num))
+                'INSERT IGNORE INTO Players VALUES (%s,%s,%s,%s,%s,%s,%s,%s)',
+                (Match_ID, inning, Batting_team, Bowling_team, over, ball, batsman, non_striker, bowler))
+            #cur.execute(
+            #    'INSERT IGNORE INTO Localization(Localization_ID,Biological_Process_Loc,Cellular_Component_Loc,Molecular_Function) VALUES (%s,%s,%s,%s)',
+            #    (ID_num, bp, cc, mf))
+            #cur.execute('INSERT IGNORE INTO YeastGene_Localization(Gene_ID,Localization_ID) VALUES (%s,%s)', (gene, ID_num))
     # insertion for SC expression table
     with open("data/matches.csv", 'r') as r1:
         next(r1)
         for line in r1:
             line = line.split(',')
-            ID_num = int(line.__getitem__(0)) + 1
+            ID_num = int(line.__getitem__(0))
             ID_num = str(ID_num)
-            gene = line.__getitem__(1)
-            condit = line.__getitem__(2)
-            sc = line.__getitem__(3)
-            cur.execute('INSERT IGNORE INTO SC_Expression(Gene_ID,Condit_ID,SC_Expression) VALUES (%s,%s,%s)',
-                        (gene, condit, sc))
+            season = line.__getitem__(1)
+            city = line.__getitem__(2)
+            date = line.__getitem__(3)
+            team1 = line.__getitem__(4)
+            team2 = line.__getitem__(5)
+            toss_winner = line.__getitem__(6)
+            toss_decision = line.__getitem__(7)
+            result = line.__getitem__(8)
+
+            cur.execute('INSERT IGNORE INTO matches VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+                        (ID_num, season, city, date, team1,team2,toss_winner,toss_decision,result)))
 cnx = make_connection()
-print('hello')
 cur = cnx.cursor()
 setup_dp(cur)
 insert_data(cur)
